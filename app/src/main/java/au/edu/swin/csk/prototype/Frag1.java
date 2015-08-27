c034e7dc24f9e9ec78a7068a5a3150f924554642
fahad8yousef <fahad.yh@gmail.com>
2015-08-28 00:14:07 +1000

    init

---
+++ app/src/main/java/au/edu/swin/csk/prototype/Frag1.java
@@ -0,0 +1,57 @@
+package au.edu.swin.csk.prototype;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Frag1 extends Fragment {
+
+
+    public Frag1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag1, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    /*
+    * fragment destroy*/
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+    /*
+    * remove frag from activity */
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/au/edu/swin/csk/prototype/MainActivity.java
@@ -0,0 +1,172 @@
+package au.edu.swin.csk.prototype;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    private ListView navList;
+    private ArrayList<String> navArray;
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
+    ImageButton runCommand;
+
+
+    //private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        runCommand = (ImageButton)findViewById(R.id.runCommand);
+        runCommand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Selected Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        navList = (ListView) findViewById(R.id.nav_list);
+        navArray = new ArrayList<String>();
+        navArray.add("Koala");
+        navArray.add("Filter");
+        navArray.add("Edit");
+        navArray.add("Options");
+        //navArray.add("4");
+        //navArray.add("5");
+        navList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, navArray );
+        navList.setAdapter(adapter);
+        navList.setOnItemClickListener(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        manager = getSupportFragmentManager();
+
+        loadSelection(0);
+
+    }
+
+    private void loadSelection(int i){
+        navList.setItemChecked(i, true);
+
+        switch (i) {
+            case 0:
+                //home
+                Home home = new Home();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder,home);
+                transaction.commit();
+                break;
+            case 1:
+                Frag1 frag1 = new Frag1();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder,frag1);
+                transaction.commit();
+                break;
+            case 2:
+                Frag2 frag2 = new Frag2();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder,frag2);
+                transaction.commit();
+                break;
+            case 3:
+                Frag3 frag3 = new Frag3();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder,frag3);
+                transaction.commit();
+                break;
+            case 4:
+                Frag4 frag4 = new Frag4();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder,frag4);
+                transaction.commit();
+                break;
+            case 5:
+                Frag5 frag5 = new Frag5();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder,frag5);
+                transaction.commit();
+                break;
+
+        }
+
+    }
+
+
+    /*
+     * Deals with different orientations
+     *  */
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater mif = getMenuInflater(); //search icon
+        mif.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+        //return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }else if (id == R.id.exit_app){
+            finish();
+            return true;
+        }else if (id == android.R.id.home){
+            if (drawerLayout.isDrawerOpen(navList)) {
+                drawerLayout.closeDrawer(navList);
+            }else {
+                drawerLayout.openDrawer(navList);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        loadSelection(position);
+
+        drawerLayout.closeDrawer(navList);
+    }
+}
