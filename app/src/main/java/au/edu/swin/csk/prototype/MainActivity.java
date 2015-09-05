c49c54609d45c6b25f365c0195ed562efe843609
fahad8yousef <fahad.yh@gmail.com>
2015-09-05 14:03:46 +1000

    Major Drawer changes Documentation changes to code structure Custom Layouts added

--- app/src/main/java/au/edu/swin/csk/prototype/MainActivity.java
+++ app/src/main/java/au/edu/swin/csk/prototype/MainActivity.java
@@ -14,31 +14,97 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
 
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private DrawerLayout drawerLayout;
-    private ListView navList;
-    private ArrayList<String> navArray;
-    //private ArrayList<String> groupArray;
+    private ListView navList1;
+    private ListView navList2;
+    private ListView navList3;
     private FragmentManager manager;
-    private FragmentTransaction transaction;
     ImageButton runCommand;
     Spinner spinner;
+    LinearLayout linear;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear = (LinearLayout) findViewById(R.id.drawer_linear);
+
+        createDrawer();
+        createAddbutton();
+        manager = getSupportFragmentManager();
+        loadSelection(0);
+
+    }
+
+    private void createDrawer(){
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        //spinner
+
+        spinner = (Spinner) findViewById(R.id.spinner_group);
+        ArrayAdapter spinAdapter =  ArrayAdapter.createFromResource(this, R.array.groups, R.layout.spinner_item);
+        spinAdapter.setDropDownViewResource(R.layout.spinner_dropdown);
+        spinner.setOnItemSelectedListener(this);
+        spinner.setAdapter(spinAdapter);
+
+///////////1
+        navList1 = (ListView) findViewById(R.id.nav_list1);
+        ArrayList<String> navArray1 = new ArrayList<String>();
+        navArray1.add("Child");
+        navArray1.add("Activity Type");
+        navArray1.add("Learning Outcome");
+
+        navList1.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> listAdapter1 = new ArrayAdapter<String>(this, R.layout.row_layout_drawer, navArray1 );
+        navList1.setAdapter(listAdapter1);
+        navList1.setOnItemClickListener(this);
+
+//////////////2
+        navList2 = (ListView) findViewById(R.id.nav_list2);
+        ArrayList<String> navArray2 = new ArrayList<String>();
+        navArray2.add("Child");
+        navArray2.add("Activity");
+        navArray2.add("Learning Outcome");
+
+        navList2.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> listAdapter2 = new ArrayAdapter<String>(this,  R.layout.row_layout_drawer, navArray2 );
+        navList2.setAdapter(listAdapter2);
+        navList2.setOnItemClickListener(this);
+
+///////////////
+        navList3 = (ListView) findViewById(R.id.nav_list3);
+        ArrayList<String> navArray3 = new ArrayList<String>();
+        navArray3.add("Summary");
+        navArray3.add("Sort Order");
+        navArray3.add("Complete");
+
+        navList3.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        ArrayAdapter<String> listAdapter3 = new ArrayAdapter<String>(this,  R.layout.row_layout_drawer, navArray3 );
+        navList3.setAdapter(listAdapter3);
+        navList3.setOnItemClickListener(this);
+///////////////
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void createAddbutton(){
 
         runCommand = (ImageButton)findViewById(R.id.runCommand);
         runCommand.setOnClickListener(new View.OnClickListener() {
@@ -47,85 +113,98 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 Toast.makeText(MainActivity.this, "Selected Button", Toast.LENGTH_SHORT).show();
             }
         });
-
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open_drawer, R.string.close_drawer );
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
-        //spinner
-
-        spinner = (Spinner) findViewById(R.id.spinnerGroup);
-        ArrayAdapter spinAdapter =  ArrayAdapter.createFromResource(this, R.array.groups, android.R.layout.simple_spinner_item);
-        //spinner.setOnItemSelectedListener(this);
-        //spinner.setAdapter(spinAdapter);
-
-
-
-        navList = (ListView) findViewById(R.id.nav_list);
-        navArray = new ArrayList<String>();
-        navArray.add("Koala");
-        navArray.add("Filter");
-        navArray.add("Edit");
-        navArray.add("Options");
-        //navArray.add("4");
-        //navArray.add("5");
-        navList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, navArray );
-        navList.setAdapter(adapter);
-        navList.setOnItemClickListener(this);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
-        manager = getSupportFragmentManager();
-
-        loadSelection(0);
-
     }
 
     private void loadSelection(int i){
-        navList.setItemChecked(i, true);
+        navList1.setItemChecked(i, true);
+
+        FragmentTransaction transaction;
+        switch (i) {
+            case 0:
+                //home
+                Home home = new Home();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, home);
+                transaction.commit();
+                break;
+            case 1:
+                Frag1 frag1 = new Frag1();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, frag1);
+                transaction.commit();
+                break;
+            case 2:
+                Frag2 frag2 = new Frag2();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, frag2);
+                transaction.commit();
+                break;
+            case 3:
+                Frag3 frag3 = new Frag3();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, frag3);
+                transaction.commit();
+                break;
+        }
+
+        navList2.setItemChecked(i, true);
 
         switch (i) {
             case 0:
                 //home
                 Home home = new Home();
                 transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_holder,home);
+                transaction.replace(R.id.fragment_holder, home);
                 transaction.commit();
                 break;
             case 1:
                 Frag1 frag1 = new Frag1();
                 transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_holder,frag1);
+                transaction.replace(R.id.fragment_holder, frag1);
                 transaction.commit();
                 break;
             case 2:
                 Frag2 frag2 = new Frag2();
                 transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_holder,frag2);
+                transaction.replace(R.id.fragment_holder, frag2);
                 transaction.commit();
                 break;
             case 3:
                 Frag3 frag3 = new Frag3();
                 transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_holder,frag3);
+                transaction.replace(R.id.fragment_holder, frag3);
                 transaction.commit();
                 break;
-            case 4:
-                Frag4 frag4 = new Frag4();
-                transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_holder,frag4);
-                transaction.commit();
-                break;
-            case 5:
-                Frag5 frag5 = new Frag5();
-                transaction = manager.beginTransaction();
-                transaction.replace(R.id.fragment_holder,frag5);
-                transaction.commit();
-                break;
+        }
 
+        navList3.setItemChecked(i, true);
+
+        switch (i) {
+            case 0:
+                //home
+                Home home = new Home();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, home);
+                transaction.commit();
+                break;
+            case 1:
+                Frag1 frag1 = new Frag1();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, frag1);
+                transaction.commit();
+                break;
+            case 2:
+                Frag2 frag2 = new Frag2();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, frag2);
+                transaction.commit();
+                break;
+            case 3:
+                Frag3 frag3 = new Frag3();
+                transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragment_holder, frag3);
+                transaction.commit();
+                break;
         }
 
     }
@@ -165,10 +244,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             finish();
             return true;
         }else if (id == android.R.id.home){
-            if (drawerLayout.isDrawerOpen(navList)) {
-                drawerLayout.closeDrawer(navList);
+
+            if (drawerLayout.isDrawerOpen(linear)) {
+                drawerLayout.closeDrawer(linear);
             }else {
-                drawerLayout.openDrawer(navList);
+                drawerLayout.openDrawer(linear);
             }
         }
 
@@ -179,18 +259,15 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
         loadSelection(position);
-
-        drawerLayout.closeDrawer(navList);
+        drawerLayout.closeDrawer(linear);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-/*
         TextView group = (TextView) view;
-        Toast.makeText(this, "you selected"+ group.getText(), Toast.LENGTH_SHORT).show();
-*/
-
+        //Toast.makeText(this, "you selected"+ group.getText(), Toast.LENGTH_SHORT).show();
+        drawerLayout.closeDrawer(linear);
     }
 
     @Override
